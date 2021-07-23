7f5455cdae76e40907b6c32c49bd8f42b1aa5f19
Eric Wang <erwang1151@gmail.com>
2021-07-23 09:00:13 -0400

    Add modelA.js and modelB.js

---
+++ models/modelA.js
@@ -0,0 +1,12 @@
+var airport = {
+  name: "CMH",
+  address: "123 Airport Ave",
+  dailyDepartures: 254,
+  dailyArrivals: 235,
+  isOpen: true,
+  hasDining: true,
+  vendorList: ["Mcdonald's", "Duty Free", "Brookstone", "Burger King"],
+  airlineList: ["Delta Airlines", "American Airlines", "United Airlines"]
+}
+
+console.log(airport);
---
+++ models/modelB.js
@@ -0,0 +1,12 @@
+var vehicle = {
+  make: "Honda",
+  model: "CRV",
+  weightInLbs: 4000,
+  gasMileage: 26,
+  isOn: false,
+  hasAC: true,
+  passengers: ["Jim", "Joe", "James"],
+  previousDestinations: ["Acadia National Park", "Racquet Club of Columbus", "Wendy's"]
+}
+
+console.log(vehicle);
