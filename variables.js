71e4fcf46ba80dc06e059ea68b977e3bcba06539
Eric Wang <erwang1151@gmail.com>
2021-07-23 08:43:20 -0400

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,20 @@
+//assign a variable called famousPerson to a string value of your choosing
+var famousPerson = "Tom Hanks";
+//assign variable called isActive to boolean value
+var isActive = true;
+//re-assign famousPerson to new string
+famousPerson = "John Travolta";
+//assign a variable called num3 to a numerical value
+var num3 = 7;
+//assign variable called num9 to different numerical value
+var num9 = 18;
+//Add your num9 variable to your num3 variable and store the result in a new variable called sum
+var sum = num3 + num9;
+//Assign a variable called animals to an array of three strings
+var animals = ["Lion", "Tiger", "Cheetah"];
+//Write a line of code that removes the first element from the animals array
+animals.shift();
+//check if the first item from the array was removed
+console.log(animals);
+//Write a line of code that prints how many characters there are in the famousPerson string
+console.log(famousPerson.length);
